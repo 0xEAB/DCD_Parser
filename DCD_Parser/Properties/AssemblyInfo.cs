7d020fe5b03fdbbebea79f88e77e41e44fd21df4
0xEAB <desisma@heidel.beer>
2017-07-28 21:54:08 +0200

    Update namespace for class 'Program' in AssemblyInfo

--- DCD_Parser/Properties/AssemblyInfo.cs
+++ DCD_Parser/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using DCD_Parser;
+using DCD_Parser.CLI;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
