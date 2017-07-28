232e87b754d0165d94d7656720ad817958aaa298
0xEAB <desisma@heidel.beer>
2017-07-28 21:42:37 +0200

    Move StartupObject 'Program' to a new namespace ('CLI')

---
+++ DCD_Parser/CLI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+using CoE.em8.Core.CLI.License;
+using CoE.em8.Core.CLI.License.GNU;
+
+namespace DCD_Parser.CLI
+{
+    class Program
+    {
+        public const string APP_TITLE = "DCD_Parser";
+        public const string APP_DESCRIPTION = "An alternative server for the D Completion Daemon (DCD) powered by D_Parser.";
+        public const string APP_VERSION = "0.1.0";
+        public const string APP_COPYRIGHT = "Copyright (C) 2017  0xEAB";
+
+        public static void Main(string[] args)
+        {
+            // Print license note
+            ILicenseInfo license = new GPLv3(APP_TITLE, "v" + APP_VERSION, APP_COPYRIGHT);
+            Console.WriteLine(license.ToString());
+
+            // Print description
+            Console.WriteLine(APP_DESCRIPTION);
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+            Environment.Exit(dcd.server.Main.Main_(args));
+        }
+    }
+}
