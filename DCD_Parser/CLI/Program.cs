5696bc574e6559b8bc76252868220119a426f7b2
0xEAB <desisma@heidel.beer>
2017-07-30 01:16:25 +0200

    Extend version number capabilities

--- DCD_Parser/CLI/Program.cs
+++ DCD_Parser/CLI/Program.cs
@@ -10,13 +10,15 @@ namespace DCD_Parser.CLI
     {
         public const string APP_TITLE = "DCD_Parser";
         public const string APP_DESCRIPTION = "An alternative server for the D Completion Daemon (DCD) powered by D_Parser.";
-        public const string APP_VERSION = "0.1.0";
+        public const string APP_VERSION_NUMBER = "0.1.0";
+        public const string APP_VERSION_SUFFIX = "-alpha";
+        public const string APP_VERSION_STRING = "v" + APP_VERSION_NUMBER + APP_VERSION_SUFFIX;
         public const string APP_COPYRIGHT = "Copyright (C) 2017  0xEAB";
 
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             // Print license note
-            ILicenseInfo license = new GPLv3(APP_TITLE, "v" + APP_VERSION, APP_COPYRIGHT);
+            ILicenseInfo license = new GPLv3(APP_TITLE, APP_VERSION_STRING, APP_COPYRIGHT);
             Console.WriteLine(license.ToString());
 
             // Print description
@@ -25,7 +27,7 @@ namespace DCD_Parser.CLI
 
             Console.ReadLine();
 
-            Environment.Exit(dcd.server.Main.Main_(args));
+            return dcd.server.Main.Main_(args);
         }
     }
 }
